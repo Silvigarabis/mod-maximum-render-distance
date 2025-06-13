fe4cb8f54352ce0b58d889d52cb3a88610a6ff16
Silvigarabis <Silvigarabis@outlook.com>
2025-06-13 19:59:35 +0800

    chore: 移除 Sodium 兼容混入类中未使用的导入语句

--- src/client/java/me/silvigarabis/mod_maximum_distance/mixin/client/compatibility/SodiumGameOptionsMixin.java
+++ src/client/java/me/silvigarabis/mod_maximum_distance/mixin/client/compatibility/SodiumGameOptionsMixin.java
@@ -1,27 +1,14 @@
 package me.silvigarabis.mod_maximum_distance.mixin.client.compatibility;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
-import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
-import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
-import me.jellysquid.mods.sodium.client.gui.options.Option;
-import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
-
-import org.objectweb.asm.Opcodes;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
 
 import me.silvigarabis.mod_maximum_distance.ModMaximumDistanceModClient;
 
